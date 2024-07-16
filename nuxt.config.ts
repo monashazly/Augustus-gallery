ea9fdf9b7c8035c6a9880e6a80bebdd783873554
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2024-07-16 15:44:19 +0300

    setup nuxt image configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/apollo"
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/apollo", "@nuxt/image"],
   apollo: {
     clients: {
       default: {
         httpEndpoint: 'https://graphqlzero.almansi.me/api',
       }
     }
+  },
+  image: {
+    domains: ['graphqlzero.almansi.me']
   }
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,28 @@
 <script setup>
+const query = gql`
+  query FetchAlbums {
+  albums {
+    data {
+      photos {
+        data {
+          url,
+          id,
+          title,
+          thumbnailUrl
+        }
+      }
+    }
+  }
+}
 
+`
+const { data } = await useAsyncQuery(query)
 </script>
 
 <template>
     <div>
-    hi
+        {{ data.albums.data }}
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
